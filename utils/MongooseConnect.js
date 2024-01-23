51b906df970614aa262d1f0bfbb1f160c8afe2cc
hassan <idreesadam200@gmail.com>
2024-01-23 15:12:57 +0100

    add mongodb cluster

--- utils/MongooseConnect.js
+++ utils/MongooseConnect.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectMongoose = async () =>{
     try {
-        await mongoose.connect('mongodb://localhost/vehicle')
+        await mongoose.connect(
+          "mongodb://localhost/vehicle" ||
+            "mongodb+srv://idreesadam200:iredox@cluster0.jcxb8on.mongodb.net/?retryWrites=true&w=majority"
+        );
         console.log('connect to mongoose')
     } catch (err) {
         console.log(err)
