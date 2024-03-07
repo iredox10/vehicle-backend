7f3c9e57f59ee91e61218aa97a9b67d7e17fbd9b
Idris adam <idreesadam200@gmail.com>
2024-03-07 14:40:54 +0100

    Update MongooseConnect.js

--- utils/MongooseConnect.js
+++ utils/MongooseConnect.js
@@ -5,8 +5,8 @@ dotenv.config()
 const connectMongoose = async () =>{
     try {
         const res = await mongoose.connect(
-            // "mongodb+srv://idreesadam200:iredox@cluster0.jcxb8on.mongodb.net/?retryWrites=true&w=majority" ||
-            'mongodb://localhost/steadfast-vehicle-app'
+             "mongodb+srv://idreesadam200:iredox@cluster0.jcxb8on.mongodb.net/?retryWrites=true&w=majority" 
+            //'mongodb://localhost/steadfast-vehicle-app'
         );
         console.log('connect to mongoose')
     } catch (err) {
