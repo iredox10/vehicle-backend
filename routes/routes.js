3f734f66daa34465991290809fd7e32ea44eb1e0
Idris adam <idreesadam200@gmail.com>
2024-03-08 15:27:54 +0100

    Update routes.js  -  download file

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,24 @@ import express from 'express'
 import * as controller from '../controllers/controllers.js'
 const route = express.Router()
 
-route.post('/',(req,res) => res.json(req.body))
+// route.post('/',(req,res) => res.json(req.body))
+route.post('/', async (req,res)=>{
+    const body = json.stringify(req.body)
+    try {
+       const file = await fs.writefile('./file.txt',body) 
+       console.log('file created');
+       res.download('../backend/file.txt', (err)=>{
+       if(err) {
+       console.log(err);
+       }else{
+       console.log('downloaded');
+       }
+    })
+    } catch (err) {
+        res.json(err)
+    }
+})
+
 route.get('/', (req,res) => res.json("welcome"))
 route.post("/payment", controller.payment);
 route.post('/register', controller.register)
