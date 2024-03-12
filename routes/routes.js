3824d0b5f2634398e4a2d2cc546278cd5cbdb09a
Idris adam <idreesadam200@gmail.com>
2024-03-12 15:38:48 +0100

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -5,20 +5,41 @@ import path from 'node:path'
 import * as fs from 'node:fs/promises'
 
 const route = express.Router()
-
 route.post('/', async (req,res)=>{ 
+    
     const body = JSON.stringify(req.body)
     try {
+        const user = await User.create({
+            transactionRef: req.body.TransactionRef,
+            payerRefNo: req.body.PayerRefNo,
+            paymentRef: req.body.paymentRef,
+            licenceFee: req.body.amount,
+            paymentDate: req.body.paymentDate
+        })
        const file = await fs.writeFile('./file.txt',body) 
-       console.log('file created');
     const text = await fs.readFile('./file.txt', 'utf8')
        const dbfile = await File.create({name: file, content: text})
-    res.json(dbfile)
+    console.log(text);
+    res.json(dbfile,user)
     } catch (err) {
-        res.json(err)
+        res.json(err.message)
     }
 
 })
+
+// route.post('/', async (req,res)=>{ 
+//     const body = JSON.stringify(req.body)
+//     try {
+//        const file = await fs.writeFile('./file.txt',body) 
+//        console.log('file created');
+//     const text = await fs.readFile('./file.txt', 'utf8')
+//        const dbfile = await File.create({name: file, content: text})
+//     res.json(dbfile)
+//     } catch (err) {
+//         res.json(err)
+//     }
+
+// })
 route.get('/', (req,res) => res.json("welcome"))
 route.post("/payment", controller.payment);
 route.post('/register', controller.register)
